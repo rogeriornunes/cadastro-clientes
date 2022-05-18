3fe04b01fd7e2c5bd20796076111caf8fb8cf39a
Rogério Ricardo Nunes <rogerioricardonunes@gmail.com>
2022-05-18 18:42:25 -0300

    Commit inicial do projeto

---
+++ src/test/java/br/com/softnunes/cadastroclientes/CadastroClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.softnunes.cadastroclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
